648d66595d00912e829f725855ea60479bc580ac
Jo Wagner <jowagner@limago.de>
2024-06-05 13:05:29 +0200

    initial commit

--- Tag3_02DBDemo/src/main/java/org/example/tag3_02dbdemo/MyRunner.java
+++ Tag3_02DBDemo/src/main/java/org/example/tag3_02dbdemo/MyRunner.java
@@ -9,18 +9,25 @@ import java.sql.*;
 @Component
 public class MyRunner implements CommandLineRunner {
 
-    private static final String CONNECTION_STRING = "jdbc:h2:c:/tmp/rola" ;
+    private static final String CONNECTION_STRING = "jdbc:h2:c:/tmp/rola;AUTO_SERVER=TRUE" ;
 
     @Override
     public void run(String... args) throws Exception {
         try(Connection connection = DriverManager.getConnection(CONNECTION_STRING, "sa", "")){
             try(Statement statement = connection.createStatement()){
-                try(ResultSet resultSet = statement.executeQuery("select * from ROLA.TBL_PERSONEN")){
+                try(ResultSet resultSet = statement.executeQuery("select * from TBL_PERSONEN")){
                     ResultSetMetaData metaData = resultSet.getMetaData();
                     for (int column = 1; column <= metaData.getColumnCount() ; column++) {
-                        System.out.print(metaData.getColumnName(column) + "\t");
+                        System.out.printf("%-30s", metaData.getColumnName(column) + "\t");
                     }
                     System.out.println();
+
+                    while (resultSet.next()) {
+                        for (int column = 1; column <= metaData.getColumnCount() ; column++) {
+                            System.out.printf("%-30s", resultSet.getString(column) + "\t");
+                        }
+                        System.out.println();
+                    }
                 }
             }
         }
