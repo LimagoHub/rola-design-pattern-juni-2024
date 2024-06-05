7060e71ba4240a8b568f2fb6849f7d6828bddb76
Jo Wagner <jowagner@limago.de>
2024-06-05 14:05:38 +0200

    initial commit

--- Tag3_02DBDemo/src/main/java/org/example/tag3_02dbdemo/MyRunner.java
+++ Tag3_02DBDemo/src/main/java/org/example/tag3_02dbdemo/MyRunner.java
@@ -9,29 +9,22 @@ import java.sql.*;
 @Component
 public class MyRunner implements CommandLineRunner {
 
-    private static final String CONNECTION_STRING = "jdbc:h2:c:/tmp/rola;AUTO_SERVER=TRUE" ;
 
     @Override
     public void run(String... args) throws Exception {
-        try(Connection connection = DriverManager.getConnection(CONNECTION_STRING, "sa", "")){
-            try(Statement statement = connection.createStatement()){
-                try(ResultSet resultSet = statement.executeQuery("select * from TBL_PERSONEN")){
-                    ResultSetMetaData metaData = resultSet.getMetaData();
-                    for (int column = 1; column <= metaData.getColumnCount() ; column++) {
-                        System.out.printf("%-30s", metaData.getColumnName(column) + "\t");
-                    }
-                    System.out.println();
 
-                    while (resultSet.next()) {
-                        for (int column = 1; column <= metaData.getColumnCount() ; column++) {
-                            System.out.printf("%-30s", resultSet.getString(column) + "\t");
-
-                        }
-                        System.out.println();
-                    }
-                }
-            }
+      Table table = new Table("SELECT * from TBL_PERSONEN");
+        for (int i = 0; i < table.getColumnCount(); i++) {
+            System.out.printf("%-30s", table.getColumnName(i));
         }
+        System.out.println();
 
+        while (table.next()) {
+            for (int i = 0; i < table.getColumnCount(); i++) {
+                System.out.printf("%-30s", table.getColumn(i));
+            }
+            System.out.println();
+
+        }
     }
 }
--- Tag3_02DBDemo/src/main/java/org/example/tag3_02dbdemo/Table.java
+++ Tag3_02DBDemo/src/main/java/org/example/tag3_02dbdemo/Table.java
@@ -1,31 +1,93 @@
 package org.example.tag3_02dbdemo;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Table implements AutoCloseable{
+    private static final String CONNECTION_STRING = "jdbc:h2:c:/tmp/rola;AUTO_SERVER=TRUE" ;
+    private final String select;
+    private Connection connection = null;
+    private Statement statement = null;
+    private ResultSet resultSet = null;
+    private ResultSetMetaData resultSetMetaData = null;
+    private Map<String, String> row = null;
+    private int columnCount = -1;
 
     public Table(String select) {
+        this.select = select;
     }
 
     public String getColumn(int column) throws Exception{
-
+        return getColumn(getColumnName(column));
     }
     public String getColumn(String columName) throws Exception{
-
+        return getRow().get(columName);
     }
 
     public String getColumnName(int column) throws Exception{
-
+        return getResultSetMetaData().getColumnName(column + 1);
     }
 
     public int getColumnCount() throws Exception{
-
+        if(columnCount == -1)
+            columnCount = getResultSetMetaData().getColumnCount();
+        return columnCount;
     }
 
     public boolean next() throws Exception{
+        if(resultSet.next()) {
+            dx();
+            return true;
+        }
+        return false;
+    }
 
+    private void dx() throws Exception{
+        for(int i = 0; i < getColumnCount(); i++) {
+            getRow().put(getColumnName(i), getResultSet().getString(getColumnName(i)));
+        }
     }
 
     @Override
     public void close() throws Exception {
+        if(resultSet != null) resultSet.close();
+        if(statement != null) statement.close();
+        if(connection != null) connection.close();
 
     }
+
+    private Connection getConnection() throws Exception{
+        if(connection == null)
+            connection = DriverManager.getConnection(CONNECTION_STRING,"sa","");
+        return connection;
+    }
+
+    private Statement getStatement() throws Exception{
+        if(statement == null)
+            statement = getConnection().createStatement();
+        return statement;
+    }
+
+    private ResultSet getResultSet() throws Exception{
+        if(resultSet == null)
+            resultSet = getStatement().executeQuery(select);
+        return resultSet;
+    }
+
+    private ResultSetMetaData getResultSetMetaData() throws Exception {
+        if (resultSetMetaData == null)
+            resultSetMetaData = getResultSet().getMetaData();
+        return resultSetMetaData;
+    }
+
+    private Map<String, String> getRow() throws Exception{
+        if(row == null) {
+            row = new HashMap<>();
+            for (int i = 0; i < getColumnCount(); i++) {
+                row.put(getColumnName(i), "");
+            }
+        }
+        return row;
+    }
+
 }
